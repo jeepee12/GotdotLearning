bc827640b7c3e8ce1568af9259e3a55a0ab21ad3
jeepee12 <jeepee.12@gmail.com>
2024-08-03 16:13:39 -0400

    Birdy going up on space

--- Nonkelconke/Birdy.cs
+++ Nonkelconke/Birdy.cs
@@ -11,6 +11,10 @@ public partial class Birdy : RigidBody2D
 
 	[Export]
 	private float airFriction = 5;
+
+	[Export]
+	private float jumpPower = 5000;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -24,5 +28,10 @@ public partial class Birdy : RigidBody2D
 		float airResistance = LinearVelocity.Length() * LinearVelocity.Length() * -airFriction;
 		GD.Print(airResistance);
 		ApplyForce(LinearVelocity.Normalized() * airResistance * (float)delta);
+
+		if (Input.IsActionPressed("Jump"))
+		{
+			ApplyForce(new Vector2(0.0f, (float)delta*jumpPower));
+		}
 	}
 }
