b00a39a1e4e68fe82408c40805a65c54a27d2ae9
jeepee12 <jeepee.12@gmail.com>
2024-08-03 14:36:20 -0400

    Moving forward bird and fps logs

--- Nonkelconke/Birdy.cs
+++ Nonkelconke/Birdy.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Birdy : Sprite2D
 {
+	int i = 0;
+	[Export]
+	private float speedModifier;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -12,5 +15,11 @@ public partial class Birdy : Sprite2D
 	public override void _Process(double delta)
 	{
 		int test = 32;
+		Position = new Vector2(Position.X + (float)delta*speedModifier, Position.Y);
+		++i;
+		if (i%10 == 0)
+		{
+			GD.Print(delta);
+		}
 	}
 }
